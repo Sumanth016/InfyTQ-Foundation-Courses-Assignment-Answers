affde255a1ebe3e64d3e8ee7f44e68c40e278123
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-06-06 17:02:56 +0530

    Create Assignment 9 - Level 3.py

---
+++ Programming Fundamentals using Python/Day 1/Assignment 9 - Level 3.py
@@ -0,0 +1,23 @@
+while(start != end) do
+
+if(Key_Found) then
+
+Unlock Lock
+
+else if(Diffuser_Found) then
+
+Diffuse Bombs
+
+end-if
+
+if(Path_Ahead_Available) then
+
+move forward
+
+else if(Left_Turn_Available) then
+
+Turn Left
+
+end-if
+
+end-while
