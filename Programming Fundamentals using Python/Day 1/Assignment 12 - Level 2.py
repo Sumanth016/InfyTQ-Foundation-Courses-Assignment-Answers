fb20f2750db4d9d00976a01bb9f7c680e9311817
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-06-06 17:29:21 +0530

    Create  Assignment 12 - Level 2.py

---
+++ Programming Fundamentals using Python/Day 1/Assignment 12 - Level 2.py
@@ -0,0 +1,26 @@
+import turtle               # allows us to use the turtles library
+wn = turtle.Screen()        # creates a graphics window
+wn.setup(540,508)           # set window dimension
+
+alex = turtle.Turtle()      # create a turtle named alex
+alex.shape("turtle")        # alex looks like a turtle
+alex.color('red')           # alex has a color
+
+
+alex.circle(50)       
+alex.backward(100) 
+alex.left(90)         
+alex.forward(100)             
+alex.right(90)                
+alex.forward(200)                
+alex.right(90)  
+alex.forward(100) 
+alex.right(90) 
+alex.forward(100) 
+alex.right(180)
+alex.forward(100)
+alex.right(90) 
+#Write the logic to draw the given pattern
+#Refer the statements given above to draw the pattern
+
+
