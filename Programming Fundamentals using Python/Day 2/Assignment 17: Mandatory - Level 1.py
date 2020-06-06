c07c5cd3b886c0be959d05bfab7bedcdea481a47
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-06-07 00:33:36 +0530

    Create Assignment 17: Mandatory - Level 1.py

---
+++ Programming Fundamentals using Python/Day 2/Assignment 17: Mandatory - Level 1.py
@@ -0,0 +1,13 @@
+#PF-Tryout
+
+def find_new_salary(current_salary,job_level):
+    hike = [(3,15),(4,7),(5,5)]
+    for i in hike:
+        if job_level in i:
+            new_salary = (current_salary * (100+i[1]))//100
+            return new_salary
+        else:
+            return current_salary
+
+new_salary=find_new_salary(15000,3)
+print(new_salary)
