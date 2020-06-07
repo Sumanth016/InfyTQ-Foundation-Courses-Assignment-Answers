ff4c54a2b3debcd04a967e1d35f4aa697210243b
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-06-07 14:00:13 +0530

    Create Exercise 19: Collaborative Exercise – Level 2.py

---
+++ Programming Fundamentals using Python/Day 3/Exercise 19: Collaborative Exercise – Level 2.py
@@ -0,0 +1,28 @@
+#PF-Tryout
+#Program to be tested
+
+def boarding(seat_number):
+    if(seat_number>=1 and seat_number<=25):
+        batch_no=1
+    elif(seat_number>=26 or  seat_number<=100):
+        batch_no=2
+    elif(seat_number>=101 and seat_number<=200):
+        batch_no=3
+    else:
+        batch_no=-1
+    return batch_no
+    
+#-----------------------------------------------------------------------------------------------------    
+    
+#PF-TCV-Tryout
+
+import pytest
+from <packagename>.solution import function_name
+
+def test_boarding_1():
+    result=boarding(3)
+    assert result==1
+
+def test_boarding_2():
+    result=boarding(24)
+    assert result==1
