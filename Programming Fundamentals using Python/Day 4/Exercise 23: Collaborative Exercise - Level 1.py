9301905e12416052cf5c1e5575e3e0d86fd696e1
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-06-07 16:22:29 +0530

    Create Exercise 23: Collaborative Exercise - Level 1.py

---
+++ Programming Fundamentals using Python/Day 4/Exercise 23: Collaborative Exercise - Level 1.py
@@ -0,0 +1,20 @@
+def translate(bilingual_dict,english_words_list):
+    swedish_words_list = []
+    for x in english_words_list:
+        swedish_words_list.append(bilingual_dict[x])
+
+    return swedish_words_list
+
+bilingual_dict= {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"ar"}
+english_words_list=["merry","christmas"]
+print("The bilingual dict is:",bilingual_dict)
+print("The english words are:",english_words_list)
+
+swedish_words_list=translate(bilingual_dict, english_words_list)
+print("The equivalent swedish words are:",swedish_words_list)
+
+'''
+The bilingual dict is: {'merry': 'god', 'year': 'ar', 'happy': 'gott', 'christmas': 'jul', 'new': 'nytt', 'and': 'och'}
+The english words are: ['merry', 'christmas']
+The equivalent swedish words are: ['god', 'jul']
+'''
