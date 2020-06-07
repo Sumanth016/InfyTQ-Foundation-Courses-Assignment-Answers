92ae78c132fec9c3edc68265f513f8c2008835ac
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-06-07 12:49:01 +0530

    Update Exercise 17 – Level 1.py

--- Programming Fundamentals using Python/Day 3/Exercise 17 – Level 1.py
+++ Programming Fundamentals using Python/Day 3/Exercise 17 – Level 1.py
@@ -2,10 +2,10 @@
 count=0
 i=1
 for baggage_weight in 29, 30, 31, 32, 28:
-    if(baggage_weight>=1 or baggage_weight <=30):
+    if(baggage_weight>=1 and baggage_weight <=30):
         print("Passenger",i,": Proceed for baggage check.")
-    else:
         count+=1
+    else:
         print("Passenger",i,": Maximum baggage weight allowed is 30kg.")
     i+=1
 
