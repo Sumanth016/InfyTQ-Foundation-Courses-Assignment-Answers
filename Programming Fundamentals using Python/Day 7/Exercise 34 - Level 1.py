d9b428305fab362ac6be023c5feb2de5ac72fd92
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-06-08 16:33:15 +0530

    Create Exercise 34 - Level 1.py

---
+++ Programming Fundamentals using Python/Day 7/Exercise 34 - Level 1.py
@@ -0,0 +1,8 @@
+import math
+
+def find_number_of_combination(number_of_flavours):
+    total_combination=pow(2,number_of_flavours)
+    return total_combination
+
+number_of_combination=find_number_of_combination(4)
+print(number_of_combination)
