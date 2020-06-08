f27c09379daca9b5241024667bdf96eb9de15aa9
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-06-08 17:23:21 +0530

    Create Assignment 50: Mandatory Assignment (puzzle) - Level 3.py

---
+++ Programming Fundamentals using Python/Day 7/Assignment 50: Mandatory Assignment (puzzle) - Level 3.py
@@ -0,0 +1,19 @@
+#PF-Assgn-50
+vowels=['a','e','i','o','u']
+def sms_encoding(data):
+    temp=[]
+    l=data.split(" ")
+    for word in l:
+        temp_word=[]
+        for letter in word:
+            if letter.lower() not in vowels:
+                temp_word.append(letter)
+        if len(temp_word)==0:
+            temp_word.append(word)
+        temp.append("".join(temp_word))
+    return " ".join(temp)
+        
+data="I love Python"
+print(sms_encoding(data))
+
+#I lv Pythn
